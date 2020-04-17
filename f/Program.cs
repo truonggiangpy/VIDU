12fff1f85ba180962d645e5701dcec255f850ac3
truonggiang76543 <63271620+truonggiang76543@users.noreply.github.com>
2020-04-17 21:06:35 +0700

    Add project files.

---
+++ f/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace f
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //nguyen truong giang
+
+        }
+    }
+}
